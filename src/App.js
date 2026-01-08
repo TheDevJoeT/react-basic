877bf65153a9695bc5b3802907df896764b6f2fd
JoeT1327 <thantziaung.joe631@gmail.com>
2026-01-08 14:11:34 +0630

    epi 19

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-
-import { useState } from 'react';
-import './App.css'; // this css only implies app.js which is a component 
-import Navbar from './components/Navbar/index';
-import PostList from './components/PostList/index';
-
+import { useState } from "react";
+import "./App.css"; // this css only implies app.js which is a component
+import Navbar from "./components/Navbar/index";
+import PostList from "./components/PostList/index";
+import Modal from "./components/Modal/index";
 
 function App() {
- 
-  
   let [posts, setPosts] = useState([
     {
-      id : 1,
-      title : "First Post"
+      id: 1,
+      title: "First Post",
     },
     {
-      id : 2,
-      title : "Second Post"
+      id: 2,
+      title: "Second Post",
     },
     {
-      id : 3,
-      title : "Third Post"
+      id: 3,
+      title: "Third Post",
     },
     {
-      id : 4,
-      title : "Fourth Post"
+      id: 4,
+      title: "Fourth Post",
     },
-  ])
- 
- 
+  ]);
+
   return (
     <>
-      <Navbar/>
-      <PostList posts = {posts}/>
+      <Navbar />
+      <PostList posts={posts} />
+      <Modal>
+        <h1>Zoom class is Available</h1>
+        <p>
+          Join <a href="">Here!</a>
+        </p>
+      </Modal>
     </>
-  
   );
 }
 
@@ -50,7 +51,7 @@ export default App;
 //     setCount((prevState)=> prevState +1)
 //     setCount((prevState)=> prevState +1)
 //     setCount((prevState)=> prevState +1)
-//     setCount((prevState)=> prevState +1) 
+//     setCount((prevState)=> prevState +1)
 //     // setCount(count+1) Never use this kind of thing to check
 //   }
 
@@ -62,4 +63,3 @@ export default App;
 //     </div>
 //   )
 // }
-
